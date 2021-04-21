c88f7c0b0c78e95ae04c1e56a047ef9e292e131d
LucasZamprogno <lucaszamprogno@gmail.com>
2021-04-20 22:26:34 -0700

    Add barebones backend

---
+++ Web/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("test")]
+        public SavedSong Get()
+        {
+            return new SavedSong
+            {
+                artist = "Red Hot Chili Peppers",
+                title = "Under The Bridge",
+                album = "Blood Sugar Sex Magik",
+                content = "Look for it online!"
+            };
+        }
+    }
+}
---
+++ Web/Models/SavedSong.cs
@@ -0,0 +1,10 @@
+namespace Web
+{
+    public class SavedSong
+    {
+        public string artist { get; set; }
+        public string title { get; set; }
+        public string album { get; set; }
+        public string content { get; set; }
+    }
+}
